f61c1896fc9257f564629eb4b54bb84ee145d036
Vikash Kumar <vikashcbsb@gmail.com>
2024-09-08 00:16:23 +0530

    server add 2

--- server/src/app.js
+++ server/src/app.js
@@ -4,11 +4,11 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import passport from './utils/passport.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -41,13 +41,13 @@ app.use(passport.session());
 import userRouter from './routes/user.route.js';
 app.use('/api/v1/users', userRouter);
 
-// Serve the client app from the correct location
-app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+// // Serve the client app from the correct location
+// app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-// Handle all routes by serving `index.html` for single-page applications
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
-});
+// // Handle all routes by serving `index.html` for single-page applications
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+// });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
