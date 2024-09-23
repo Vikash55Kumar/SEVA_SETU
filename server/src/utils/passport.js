438bd56d9494bf98f44018477d75ed902a5d0655
Vikash Kumar <vikashcbsb@gmail.com>
2024-09-23 08:28:18 +0530

    googleUrl updated

--- server/src/utils/passport.js
+++ server/src/utils/passport.js
@@ -8,7 +8,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL || "https://seva-setu.netlify.app/api/v1/users/auth/google/callback",
   passReqToCallback: true
 },
 async function(request, accessToken, refreshToken, profile, done) {
