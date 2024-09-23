046d9377223831c6a537ee0ba06ca9a893cd124c
Vikash Kumar <vikashcbsb@gmail.com>
2024-09-23 16:23:36 +0530

    googleUrl updated20

--- server/src/utils/passport.js
+++ server/src/utils/passport.js
@@ -90,7 +90,7 @@ async function(request, accessToken, refreshToken, profile, done) {
           url: profile.photos[0].value,
           filename: `google${profile.id}`,
         },
-        ...(profile.phoneNumber ? { phoneNumber: profile.phoneNumber } : {}),
+        phoneNumber: profile.phoneNumber || undefined,
         password: null, // Set password to null for Google OAuth users
       });
 
