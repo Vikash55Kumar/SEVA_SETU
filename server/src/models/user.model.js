ab84781a316bcee81b46778b829501e43649b73a
Vikash Kumar <vikashcbsb@gmail.com>
2024-10-11 20:06:55 +0530

    google update13

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema(
         },
         phoneNumber: {
             type: String,
-            default: "null",
+            default: null,
           },
         password: {
             type: String,
